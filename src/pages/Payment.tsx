cd9e8eabd94e9ef88d4750d4a0a8ceba3fee4397
the-scarlet-spyder <adrian.m.adepoju@gmail.com>
2025-06-08 19:33:11 +0100

    Update Payment.tsx

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -13,7 +13,7 @@ const Payment = () => {
   const { user } = useAuth();
   
   // Set your price in the lowest currency unit (e.g., kobo for NGN, cents for USD)
-  const price = 2500000; // ₦25,0000.00
+  const price = 500000; // ₦5,0000.00
   
   const handlePaymentSuccess = () => {
     navigate('/welcome');
